cfbd7f21484e564df7b32545ba3dc0250b3c63fd
Hasham804 <hasahm147@gmail.com>
2024-08-11 09:14:17 -0700

    "complete"

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -54,7 +54,7 @@ const about = () => {
             </div>
             <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light dark:border-light dark:bg-dark  p-8 xl:col-span-4 md:order-1 md:col-span-8 '>
                 <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light'/>
-                <Image src={profilePic} alt='Mudassar' className='w-full h-auto rounded-2xl'
+                <Image src={profilePic} alt='Hasham' className='w-full h-auto rounded-2xl'
                 priority
                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 />
